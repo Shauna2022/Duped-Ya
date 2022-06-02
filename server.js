c5d57cb5ea89457315a017ca711e61e864edc393
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-06-01 23:33:04 -0400

    connected MongoDB+added .env

--- server.js
+++ server.js
@@ -4,23 +4,33 @@
 const express = require('express');
 const app = express();
 const productsController = require('./controllers/products.js')
-
+require('dotenv').config()
+const mongoose = require('mongoose');
 ////////////////////////////////////////////////////////////
 //                      SET UP DATEBASE
-////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////// 
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	
+});
 
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 ////////////////////////////////////////////////////////////
 //                      MIDDLEWARE
 ////////////////////////////////////////////////////////////
-app.use('/products', productssController);
+app.use('/products', productsController);
 
 ////////////////////////////////////////////////////////////
 //                      ROUTES
 ////////////////////////////////////////////////////////////
 ////////////////////////INDEX ////////////////////////
 app.get('/', (req, res)=> {
-    res.render('index.ejs')
+    res.send('index.ejs')
 })
 
 ////////////////////////////////////////////////////////////
